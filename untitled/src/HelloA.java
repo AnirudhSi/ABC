879642583d181863a7531160b1d895b5cb1dea72
asivapurapu <asivapurapu@lb.com>
2016-08-19 11:51:16 -0400

    HelloA 1.1 added to A_Feature_1.

--- untitled/src/HelloA.java
+++ untitled/src/HelloA.java
@@ -1,8 +1,6 @@
-/**
- * Created by asivapurapu on 8/19/2016.
- */
+
 public class HelloA {
     public static void main(String args[]){
-        System.out.println("HelloA");
+        System.out.println("HelloA Feature 1.1");
     }
 }
