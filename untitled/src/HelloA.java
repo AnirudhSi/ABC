87a208d09ee0311e00fa2f9c83e0c9b8e81cc23b
asivapurapu <asivapurapu@lb.com>
2016-08-19 13:39:12 -0400

    All smalls on feature A.

--- untitled/src/HelloA.java
+++ untitled/src/HelloA.java
@@ -1,6 +1,6 @@
 
 public class HelloA {
     public static void main(String args[]){
-        System.out.println("Hello Feature A Branch");
+        System.out.println("hello feature b branch");
     }
 }
